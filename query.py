c76f173d579e505439971b6184f0b69be3907514
Jion Fairchild <jion.fairchild@gmail.com>
2015-11-25 10:54:40 -0500

    changed url global

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
 import json, urllib2
-domain = "www.thebluealliance.com"
-api = "/api/v2/%s"
+domain = "http://www.dictionaryapi.com/"
+api = "/api/v1/references/sd4/xml/test?key=bc493743-092d-4f6f-8716-2d3097d603a9"
 key = "b4e2408f-1ee2-4917-95d9-c0ff9eff196f"
