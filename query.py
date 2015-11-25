489f10c6bc73d16369b21d297ab7813c0152dc34
Jion Fairchild <jion.fairchild@gmail.com>
2015-11-25 11:25:51 -0500

    fixed key

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
 import json, urllib2
-key = "b4e2408f-1ee2-4917-95d9-c0ff9eff196f"
+key = "bc493743-092d-4f6f-8716-2d3097d603a9"
 domain = "http://www.dictionaryapi.com/"
 api = "/api/v1/references/sd4/xml/%s?key="+key
 
