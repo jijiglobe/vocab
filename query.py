568da02f483edabfb23a1bcc687d6ccc3141c234
Jion Fairchild <jijiglobe@yahoo.com>
2015-11-25 11:24:38 -0500

    added getXML function

--- query.py
+++ query.py
@@ -1,4 +1,19 @@
 import json, urllib2
-domain = "http://www.dictionaryapi.com/"
-api = "/api/v1/references/sd4/xml/test?key=bc493743-092d-4f6f-8716-2d3097d603a9"
 key = "b4e2408f-1ee2-4917-95d9-c0ff9eff196f"
+domain = "http://www.dictionaryapi.com/"
+api = "/api/v1/references/sd4/xml/%s?key="+key
+
+hdr = {
+    'User-Agent': 'Mozilla',
+    'Accept': 'xml',
+    'Accept-Language': 'en-us',
+}
+
+def getXML(word):
+    req = urllib2.Request((domain+api) % word,headers=hdr)
+    request = urllib2.urlopen(req)
+    result = request.read()
+    
+    return result
+
+print getXML(raw_input())
