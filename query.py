f0d5238455ab3bc374e952a706a99c8166cad4ab
Jion Fairchild <jion.fairchild@gmail.com>
2015-11-29 14:35:08 -0500

    :smile:

--- query.py
+++ query.py
@@ -1,7 +1,12 @@
 import json, urllib2
-key = "bc493743-092d-4f6f-8716-2d3097d603a9"
+from temboo.Library.Wordnik.Word import GetEtymology
+from temboo.core.session import TembooSession
+
+websterKey = "bc493743-092d-4f6f-8716-2d3097d603a9"
+wordnikKey = "e832a6ca17874df39200204fc350926dc62d219664357eb3e"
+
 domain = "http://www.dictionaryapi.com/"
-api = "/api/v1/references/sd4/xml/%s?key="+key
+api = "/api/v1/references/sd4/xml/%s?key="+websterKey
 
 hdr = {
     'User-Agent': 'Mozilla',
@@ -25,4 +30,33 @@ def findTag(xml,tag):
     tag = "</"+tag[1:]
     xml[1] = xml[1].split(tag)
     return xml[1][0]
-print findTag(breakIntoElements(getXML(raw_input()))[1],"<fl>")
+
+def getPartOfSpeech(xml,word):
+    xml = breakIntoElements(xml)
+    return findTag(xml[1],"<fl>")
+
+#print getPartOfSpeech(getXML("test"),"test")
+
+def getEtymology(word):
+    #Code snippets stolen from Temboo.com
+
+    # Create a session with your Temboo account details
+    session = TembooSession("jijiglobe", "myFirstApp", "44421ac2aaac44d9961d65193aa6c26d")
+    
+    # Instantiate the Choreo
+    getEtymologyChoreo = GetEtymology(session)
+    
+    # Get an InputSet object for the Choreo
+    getEtymologyInputs = getEtymologyChoreo.new_input_set()
+    
+    # Set the Choreo inputs
+    getEtymologyInputs.set_APIKey(wordnikKey)
+    getEtymologyInputs.set_Word(word)
+    
+    # Execute the Choreo
+    getEtymologyResults = getEtymologyChoreo.execute_with_results(getEtymologyInputs)
+    
+    # Print the Choreo outputs
+    return getEtymologyResults.get_Response()
+
+print getEtymology(raw_input())
